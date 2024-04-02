743a96f98b6836e5756467d1d428ee57010aa352
allerman <allerman3301@gmail.com>
2024-04-01 21:05:46 -0300

    adicionando tratamento de erro a entrada de string

--- adivinhe.cpp
+++ adivinhe.cpp
@@ -1,21 +1,33 @@
 #include<iostream>
+#include <limits>
 using namespace std;
 
 int main(){    
 	cout << "*********************" << endl;
 	cout << "Bem vindo ao jooginho" << endl;
+	cout << "Adivinhe o numero" << endl;
 	cout << "*********************" << endl;
 
 	int nSecreto = 42;
 	int chute;	
-	
+
+while(true){
 	cout << "Qual seu chute?" << endl;
 	cin >> chute;
-	
-	cout << "Voce tem certeza que o valor é " << chute << "?" <<endl;
+
+	if(cin.fail()){
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	 	cout << "Erro: Digite apenas números inteiros." << endl;
+    } 
+	else {
+    	break;
+    }
+}
+	//cout << "Voce tem certeza que o valor é " << chute << "?" <<endl;
  	
 	if(chute == nSecreto){	
-	cout << "Voce acertou secreto é " << nSecreto << ". nao conte a ninguém" << endl;	
+	cout << "Voce acertou secreto é " << nSecreto << ". Nao conte a ninguém!" << endl;	
 	}
 	else if(chute > nSecreto){
 	cout << "Chutou auto, tente mais baixo" << endl;
@@ -24,4 +36,5 @@ int main(){
 	cout << "Chutou baixo, tente mais auto" << endl;
 	}
 	
+	return 0;
 }
